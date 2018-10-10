580feaa678a50bf56e12eac94dd24276ccc6ff8a
Sahil <shlbnsl843@gmail.com>
2018-10-11 00:33:58 +0530

    Add hostname to IP address conversion programs

--- A5-Socket-Programming-I/q5_ipaddr_to_hostname.c
+++ A5-Socket-Programming-I/q5_ipaddr_to_hostname.c
@@ -18,26 +18,29 @@ int main(int argc, char **argv) {
 	struct hostent *host;
 
 	if (argc < 2) {
-        printf("Usage: ip <hostname1 hostname2 hostname3 ...>\n");
+        printf("Usage: host <IPA1 IPA2 IPA3 ...>\n");
         exit(EXIT_FAILURE);
 	}
 
 	for (int i = 1; i < argc; i ++) {
 		// prototype:
-		// struct hostent *gethostbyname (const char *hostname)
-		host = gethostbyname(argv[i]);
-
-		if (!host) {
-	        printf("Lookup Failed: %s\n", hstrerror(h_errno));
-	        return 0;
-		}
-		printf("host_name: %s\n", host->h_name);
+		// struct hostent *gethostbyaddr (const char *addr, size_t len, int family)
+		struct in_addr ipa;
+		inet_aton(argv[i], &ipa);
+		host = gethostbyaddr(&ipa, sizeof(ipa), AF_INET);
 
 		int j = 0;
 		while(host->h_addr_list[j] != NULL) {
 	        printf("host_addr_list[%d]: %s\n", j, inet_ntoa( (struct in_addr) * (struct in_addr *)host->h_addr_list[j] ) );
 	        j++;
 		}
+
+		if (!host) {
+	        printf("Lookup Failed: %s\n", hstrerror(h_errno));
+	        return 0;
+		}
+		printf("host_name: %s\n\n", host->h_name);
+
 	} 
 
 	return 0;
