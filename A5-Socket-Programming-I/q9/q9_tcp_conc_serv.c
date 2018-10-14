1be0ba55b430a9299d95f306b1443d7b93217844
Sahil <shlbnsl843@gmail.com>
2018-10-14 22:40:42 +0530

    Add comments in Q7 and modify for Q9 concurrent server

--- A5-Socket-Programming-I/q9/q9_tcp_conc_serv.c
+++ A5-Socket-Programming-I/q9/q9_tcp_conc_serv.c
@@ -51,59 +51,46 @@ void str_daytime(int sockfd) {
 
 int main(int argc, char **argv) {
 
- 	// socket descriptors
 	int listenfd, connfd;
-
-	// length for the client structure
+	pid_t childpid;
 	socklen_t clilen;
-
-	// client and server sockaddr_in structures
 	struct sockaddr_in cliaddr, servaddr;
 
-	// the socket call to get the listening socket descriptor
 	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
 		perror("Socket error");
 		exit(0);
 	}
 
- 	// zeroize the servaddr structure
 	bzero(&servaddr, sizeof(servaddr));
-
-	// populate the values in the servaddr structure
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(SERVPORT);
 
-	// bind the listening socket descriptor to the servaddr structure
 	if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) == -1) {
 		perror("Bind error");
 		exit(0);
 	}
 
-	// listen atmost to LISTENQ connections 
 	if ((listen(listenfd, LISTENQ)) == -1) {
 		perror("Listen error");
 		exit(0);
 	}
 
-	// iterative server
-	// so keep processing requests
-	// automatically maintains a queue of requests from different clients
+	// concurrent server
+	// multiple clients requests can be processed at the same time
 	for ( ; ; ) {
 		clilen = sizeof(cliaddr);
-
-		// accept the connection from client
 		if ((connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen)) == -1) {
 			perror("Accept error");
 			exit(0);
 		}
-
-		// process the daytime requests of the client
-		str_daytime(connfd);
-
-		// close the connected socket 
-		close(connfd); 
+		if ((childpid = fork()) == 0) {
+			// this is a child process
+			close(listenfd); // child closes listening descriptor
+			str_daytime(connfd);
+			exit(0);
+		}
+		close(connfd); // parent closes connected (accepted) socket
 	}
 
-	return 0;
 }
--- A5-Socket-Programming-I/q9/q9_tcpdaytimecli.c
+++ A5-Socket-Programming-I/q9/q9_tcpdaytimecli.c
@@ -44,7 +44,7 @@ void str_daytime(FILE *fp, int sockfd) {
 // function to display the ip address and port number of the server
 int show_server_details(struct sockaddr_in serv_addr) {
 	uint16_t port_no = ntohs(serv_addr.sin_port);
-	
+
 	struct hostent *host;
 	host = gethostbyaddr(&serv_addr.sin_addr, sizeof(serv_addr.sin_addr), AF_INET);
 	if (!host) {
@@ -61,39 +61,30 @@ int main(int argc, char **argv) {
     // socket internet address structure
 	struct sockaddr_in servaddr;
     
-    // usage for the program
 	if (argc != 2) {
 		perror("usage: tcpcli <IPaddress>");
 		exit(0);
 	}
 
-	// the socket call to get the socket descriptor for the client
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
 		perror("Socket creation error!");
 		exit(0);
 	}
 
-	// zeroise the servaddr structure
 	bzero(&servaddr, sizeof(servaddr));
-
-	// populate the fields of sockaddr
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(SERV_PORT);
-	// convert IP from ASCII to NBO and also populate in the servaddr.sin_addr field 
-	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr); 
 	
-	// connect to the server
 	if ((connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr))) == -1) {
 		perror("Connect error"); 
 		exit(0);
 	}
 
-	// show the hostname and the port number of the server
 	if (show_server_details(servaddr) == 1) {
-		str_daytime(stdin, sockfd);		
+		str_daytime(stdin, sockfd);
 	}
 
-	// close the socket descriptor
 	close(sockfd);
     return 0;
 }
